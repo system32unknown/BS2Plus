cafe808e96744e8d044c2ce8732b5383cac32938
Altertoriel <79248736+system32unknown@users.noreply.github.com>
2026-03-11 23:26:04 +0900

    m

--- src/output.cpp
+++ src/output.cpp
@@ -3,28 +3,14 @@
 
 int error(char *text, int owner)
 {
-	if (owner)
-	{
-		sendowner(text, owner);
-	}
-	else
-	{
-		addConsoleTextLine(text);
-		return 0;
-	}
+	if (owner) sendowner(text, owner);
+	else addConsoleTextLine(text);
 	return 0;
 }
 
 int print(char *text, int owner, int size)
 {
-	if (owner)
-	{
-		sendowner(text, owner, size);
-	}
-	else
-	{
-		addConsoleTextLine(text);
-		return 0;
-	}
+	if (owner) sendowner(text, owner, size);
+	else addConsoleTextLine(text);
 	return 0;
 }
